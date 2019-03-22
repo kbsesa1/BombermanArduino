8c6f2786c064c6d882bee5be240fa7bce40067af
Joost <joosthartkamp@hotmail.com>
2019-03-22 12:38:09 +0100

    git clean

--- PLAYER2/Debug/Adafruit-GFX-Library-master/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\HP\AppData\Local\Temp\VMBuilds\PLAYER2\uno\Debug\Adafruit-GFX-Library-master\glcdfont.c.o: \
- C:\Users\HP\Documents\Arduino\libraries\Adafruit-GFX-Library-master\glcdfont.c
